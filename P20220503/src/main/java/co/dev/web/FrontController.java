aea498759eed05e09590955b6cee03c5fdb4a2f3
hjj <hjj3343@gmail.com>
2022-05-03 17:40:08 +0900

    aa

---
+++ P20220503/src/main/java/co/dev/web/FrontController.java
@@ -0,0 +1,42 @@
+package co.dev.web;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.filters.SetCharacterEncodingFilter;
+
+public class FrontController extends HttpServlet {
+	HashMap<String, Control> list = null;
+	String charset = null;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		charset = config.getInitParameter("charset");
+		list = new HashMap<String, Control>();
+		list.put("/memberInsert.do", new MemberInsertControl()); // (호출페이지, 구현하는 클래스)
+		list.put("/memberUpdate.do", new MemberUpdateControl());
+		list.put("/memberList.do", new MemberListControl());
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.setCharacterEncoding(charset);
+		
+		
+		
+		String url =  req.getRequestURI(); // /P20220503/memberInsert.do 
+		String context = req.getContextPath(); // 프로젝트이름
+		String path = url.substring(context.length()); // memberInsert.do의 제일 마지막 부분을 담아오겠다는 의미 패키지 이후의 값을 잘라냄
+		
+		Control exeCon = list.get(path);
+		exeCon.execute(req, resp);
+	}
+}
---
+++ P20220503/src/main/java/co/dev/web/MemberUpdateControl.java
@@ -0,0 +1,31 @@
+package co.dev.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.dev.vo.MemberVO;
+
+public class MemberUpdateControl implements Control {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("수정처리하는 컨트롤입니다.");
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("passwd");
+		String nm = request.getParameter("name");
+		String em = request.getParameter("email");
+		
+		MemberVO vo = new MemberVO();
+		vo.setId(id);
+		vo.setPasswd(pw);
+		vo.setName(nm);
+		vo.setEmail(em);
+		
+		
+	}
+
+}
