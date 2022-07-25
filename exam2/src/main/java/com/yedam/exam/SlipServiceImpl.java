3cf131c8adc17f7cd075e194b2b3e5a0666503f3
jinju <hjj3343@gmial.com>
2022-07-25 19:35:13 +0900

    과제

---
+++ exam2/src/main/java/com/yedam/exam/SlipServiceImpl.java
@@ -0,0 +1,21 @@
+package com.yedam.exam;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+@Service
+public class SlipServiceImpl implements SlipService{
+
+	@Resource	SlipDAO dao;
+	
+	@Override
+	public void insertSlip(List<Slip> slip) {
+		//테이블 입력
+		for(Slip l : slip) {
+			dao.insertSlip(l);  
+		}
+	}
+
+}
